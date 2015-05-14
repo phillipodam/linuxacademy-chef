5aa2e6fd9eab05517bd1b92b7abb40c6e2fe6e7c
root <root@phillip1.(none)>
2015-05-14 20:50:07 +0000

    Updating our workstation chef-repo

--- cookbooks/apache/attributes/default.rb
+++ cookbooks/apache/attributes/default.rb
@@ -2,6 +2,8 @@ default["apache"]["sites"]["phillip2"] = { "site_title" => "Phillip2s website co
 default["apache"]["sites"]["phillip2b"] = { "site_title" => "Phillip2bs website coming soon", "port" => 80, "domain" => "phillip-odam-nitorgroup-com2b.mylabserver.com" }
 default["apache"]["sites"]["phillip3"] = { "site_title" => "Phillip3 website", "port" => 80, "domain" => "phillip-odam-nitorgroup-com3.mylabserver.com" }
 
+default["author"]["name"] = "phillip"
+
 case node["platform"]
 when "centos"
 	default["apache"]["package"] = "httpd"
---
+++ roles/base.rb
@@ -0,0 +1,3 @@
+name "base"
+description "contains recipes that should run on all nodes"
+run_list "recipe[chef-client::delete_validation]", "recipe[chef-client::cron]", "recipe[chef-client]", "recipe[localusers]"
